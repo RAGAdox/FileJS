7d9bf9d7f13fb6bfed38fd160147ab9592e4e3a3
RAGAdox <rishitech882@gmail.com>
2019-02-26 20:12:10 +0530

    File upload And Download working

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express=require('express');
 const exphbs=require('express-handlebars');
 const fileUpload = require('express-fileupload');
+const testFolder = './uploads/';
+const fs = require('fs');
+let filelist=[];
+fs.readdir(testFolder, (err, files) => {
+    files.forEach(file => {
+      //console.log(file);
+      filelist.push(file);
+    });
+  });
 const app=express();
 app.use(fileUpload());
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
+
 app.get('/download', (req, res) => res.download('./file.txt',(err)=>{
     if(err==null)
         console.log('File Transfered Successfully');
@@ -20,7 +30,7 @@ app.post('/upload', function(req, res) {
       return;
     }
   
-    console.log('req.files >>>', req.files); // eslint-disable-line
+    //console.log('req.files >>>', req.files); // eslint-disable-line
   
     sampleFile = req.files.sampleFile;
   
@@ -30,12 +40,28 @@ app.post('/upload', function(req, res) {
       if (err) {
         return res.status(500).send(err);
       }
-  
-      res.send('File uploaded to ' + uploadPath);
+      
+      //res.send('File uploaded to ' + uploadPath);
+      filelist.push(sampleFile.name);
+      res.redirect('/');
     });
   });
 app.get('/',(req,res)=>res.render('index',{
-    title:'Files App'
+    title:'Files App',
+    filelist
 }));
+
+
+
+app.get('/downloadFile/:file', (req, res) => {
+    console.log(req.params.file);
+    res.download('./uploads/'+req.params.file.substring(1),(err)=>{
+    if(err==null)
+        console.log('File Transfered Successfully');
+    else
+        console.log(err);
+})
+
+});
 const PORT=process.env.PORT||3000;
 app.listen(PORT,()=>console.log('Server Started at post : '+PORT));

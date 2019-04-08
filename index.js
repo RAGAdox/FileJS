8d2a9cca2fb3a751baa15b364d39b7f45fd705a2
RAGAdox <rishitech882@gmail.com>
2019-04-08 15:48:17 +0530

    this is a commit

--- index.js
+++ index.js
@@ -1,70 +1,68 @@
-const express = require('express');
-const exphbs = require('express-handlebars');
-const fileUpload = require('express-fileupload');
+const express = require("express");
+const exphbs = require("express-handlebars");
+const fileUpload = require("express-fileupload");
 var bodyParser = require("body-parser");
-const path=require("path");
-const testFolder = './uploads/';
-const fs = require('fs');
+const path = require("path");
+//require("./models/db");
+const testFolder = "./uploads/";
+const fs = require("fs");
 let filelist = [];
-let links=[];
+let links = [];
 fs.readdir(testFolder, (err, files) => {
-    files.forEach(file => {
-        //console.log(file);
-        filelist.push(file);
-    });
+  files.forEach(file => {
+    //console.log(file);
+    filelist.push(file);
+  });
 });
 const app = express();
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(fileUpload());
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
-app.set('view engine', 'handlebars');
-app.post('/upload', function (req, res) {
-    let sampleFile;
-    let uploadPath;
-    //console.log(req.files);
-    if (Object.keys(req.files).length == 0) {
-        res.status(400).send('No files were uploaded.');
-        return;
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+app.post("/upload", function(req, res) {
+  let sampleFile;
+  let uploadPath;
+  //console.log(req.files);
+  if (Object.keys(req.files).length == 0) {
+    res.status(400).send("No files were uploaded.");
+    return;
+  }
+
+  //console.log('req.files >>>', req.files); // eslint-disable-line
+
+  sampleFile = req.files.sampleFile;
+
+  uploadPath = __dirname + "/uploads/" + sampleFile.name;
+
+  sampleFile.mv(uploadPath, function(err) {
+    if (err) {
+      return res.status(500).send(err);
     }
 
-    //console.log('req.files >>>', req.files); // eslint-disable-line
-
-    sampleFile = req.files.sampleFile;
-
-    uploadPath = __dirname + '/uploads/' + sampleFile.name;
-
-    sampleFile.mv(uploadPath, function (err) {
-        if (err) {
-            return res.status(500).send(err);
-        }
-
-        //res.send('File uploaded to ' + uploadPath);
-        filelist.push(sampleFile.name);
-        res.redirect('/');
-    });
+    //res.send('File uploaded to ' + uploadPath);
+    filelist.push(sampleFile.name);
+    res.redirect("/");
+  });
 });
-app.get('/', (req, res) => res.render('index', {
-    title: 'Files App',
+app.get("/", (req, res) =>
+  res.render("index", {
+    title: "Files App",
     filelist,
     links
-}));
-app.post('/links',(req,res)=>
-{
-    /*res.send(req.body.name);
+  })
+);
+app.post("/links", (req, res) => {
+  /*res.send(req.body.name);
     console.log(req.body.name);*/
-    links.push(req.body.name);
-    res.redirect('/');
-    
+  links.push(req.body.name);
+  res.redirect("/");
 });
-app.get('/downloadFile/:file', (req, res) => {
-    console.log(req.params.file);
-    res.download('./uploads/' + req.params.file.substring(1), (err) => {
-        if (err == null)
-            console.log('File Transfered Successfully');
-        else
-            console.log(err);
-    })
-
+app.get("/downloadFile/:file", (req, res) => {
+  console.log(req.params.file);
+  res.download("./uploads/" + req.params.file.substring(1), err => {
+    if (err == null) console.log("File Transfered Successfully");
+    else console.log(err);
+  });
 });
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log('Server Started at post : ' + PORT));
+app.listen(PORT, () => console.log("Server Started at post : " + PORT));
